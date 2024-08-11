6bf3a33bb490f13196f90ae2ee169b0937b30af5
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-12 00:38:45 +0530

    Add pseudocode for Stock Picker

---
+++ stock-picker.rb
@@ -0,0 +1,16 @@
+def stock_picker(stock_prices)
+  # PSEUDOCODE
+  # INIT biggest profit equal to 0
+  # INIT best day to buy equal to 0
+  # INIT best day to sell equal to 0
+  # FOR every stock price in the array
+  #   FOR every stock price after the first
+  #     IF difference of stock prices between consecutive
+  #        days is greater than the biggest profit made THEN
+  #       SET biggest profit equal to the difference
+  #       SET best day to buy equal to the first loop index
+  #       SET best day to sell equal to the second loop index
+  #     ENDIF
+  #   ENDFOR
+  # ENDFOR
+end
