315280d5c652b3c467f140c60abf438d3d282f59
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-12 01:02:38 +0530

    Add the algorithm for Stock Picker

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,16 +1,20 @@
 def stock_picker(stock_prices)
-  # PSEUDOCODE
-  # INIT biggest profit equal to 0
-  # INIT best day to buy equal to 0
-  # INIT best day to sell equal to 0
-  # FOR every stock price in the array
-  #   FOR every stock price after the first
-  #     IF difference of stock prices between consecutive
-  #        days is greater than the biggest profit made THEN
-  #       SET biggest profit equal to the difference
-  #       SET best day to buy equal to the first loop index
-  #       SET best day to sell equal to the second loop index
-  #     ENDIF
-  #   ENDFOR
-  # ENDFOR
+  biggest_profit = 0
+  best_buy_day = 0
+  best_sell_day = 0
+
+  stock_prices.each_with_index do |buy_price, buy_day|
+    stock_prices.each_with_index do |sell_price, sell_day|
+      next if buy_day >= sell_day
+      if sell_price - buy_price > biggest_profit
+        biggest_profit = sell_price - buy_price
+        best_buy_day = buy_day
+        best_sell_day = sell_day
+      end
+    end
+  end
+
+  [best_buy_day, best_sell_day]
 end
+
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
